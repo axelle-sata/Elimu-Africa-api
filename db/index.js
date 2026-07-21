5abaf5f40ef099ead234397c01b7557255a4ea53
axelle-sata <axellediatta32@gmail.com>
2026-07-21 20:59:51 +0000

    enable postgres ssl

--- db/index.js
+++ db/index.js
@@ -2,15 +2,26 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-  host:     process.env.DB_HOST,
-  port:     process.env.DB_PORT,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   database: process.env.DB_NAME,
-  user:     process.env.DB_USER,
+  user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
+
+// Test de connexion
 pool.connect()
-  .then(() => console.log("✅ Connecté à PostgreSQL"))
-  .catch(err => console.error("❌ Erreur connexion DB:", err.message));
+  .then(client => {
+    console.log("✅ Connecté à PostgreSQL");
+    client.release();
+  })
+  .catch(err => {
+    console.error("❌ Erreur connexion DB:", err.message);
+  });
+
 
 module.exports = pool;
